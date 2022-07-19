95485eaefbf104846fbc7874267c154d1fc18d83
supercode <getlucked@gmx.de>
2022-07-19 14:38:31 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+let output = document.getElementById("output")
+
+function multiply() {
+    const number = document.getElementById("number").value
+    output.innerHTML = number * 2
+
+}
+
+
+
+
+
+
